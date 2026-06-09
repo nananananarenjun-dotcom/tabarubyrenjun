c4d6a92abe3ad2bcd7deb7a21abde6b4958e3b25
cornel-s-k <cornel.lydia@gmail.com>
2026-06-09 19:14:37 +0700

    test signature

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Kita biarkan rute awal kosong atau sekadar pesan bahwa API berjalan
 Route::get('/', function () {
     return response()->json(['message' => 'Backend API Galeri Sabira Aktif!']);
+});
+
+Route::get('/test-signature', function () {
+    return response()->json([
+        'app_url' => config('app.url'),
+        'current_url' => request()->fullUrl(),
+        'https' => request()->secure(),
+        'host' => request()->getHost(),
+    ]);
 });
