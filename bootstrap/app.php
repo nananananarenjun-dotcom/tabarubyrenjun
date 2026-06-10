bcf2b51878cfd06608bde58c1f70b3d55d95ee69
cornel-s-k <cornel.lydia@gmail.com>
2026-06-10 08:37:02 +0700

    buatnetlify

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,11 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->trustProxies(
-        at: '*'
-    );
+    $middleware->trustProxies(at: '*');
 
     $middleware->statefulApi();
+
+    $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
 })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->render(function (AuthenticationException $e, Request $request) {
